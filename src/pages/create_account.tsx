de97c2668e9e262684af6294151ce851942938a6
Romario Negreiros <nromario482@gmail.com>
2022-06-16 21:50:07 -0300

    feat: add inputs interface + add react hook form

--- src/pages/create_account.tsx
+++ src/pages/create_account.tsx
@@ -1,14 +1,60 @@
+import { useForm, SubmitHandler } from 'react-hook-form'
+
 import Link from 'next/link'
 
 import type { NextPage } from 'next'
 
+interface Inputs {
+  email: string
+  name: string
+}
+
 const CreateAccount: NextPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<Inputs>()
+
+  const onSubmit: SubmitHandler<Inputs> = data => {
+    console.log(data)
+  }
+
   return (
     <section className="form_wrapper">
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <section className="form_inner_content_wrapper">
-          <input type="email" placeholder="Email" />
-          <input placeholder="Name" />
+          <div>
+            <input
+              type="email"
+              placeholder="Email"
+              {...register('email', {
+                required: 'Email is required',
+                pattern: {
+                  value: /^.+[@]{1}[aA-zZ]+[.]{1}.+$/,
+                  message: 'Invalid email format. example@mail.com'
+                }
+              })}
+            />
+            <p>{errors.email?.message}</p>
+          </div>
+          <div>
+            <input
+              placeholder="Name"
+              {...register('name', {
+                required: 'Name is required',
+                minLength: {
+                  value: 4,
+                  message: 'Minimum 4 characters'
+                },
+                maxLength: {
+                  value: 20,
+                  message: 'Maximum of 20 characters'
+                }
+              })}
+            />
+            <p>{errors.name?.message}</p>
+          </div>
         </section>
         <section className="form_inner_content_wrapper">
           <button>Submit</button>
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,13 +1,42 @@
+import { useForm, SubmitHandler } from 'react-hook-form'
+
 import Link from 'next/link'
 
 import type { NextPage } from 'next'
 
+interface Inputs {
+  email: string
+}
+
 const Login: NextPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<Inputs>()
+
+  const onSubmit: SubmitHandler<Inputs> = data => {
+    console.log(data)
+  }
+
   return (
     <section className="form_wrapper">
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <section className="form_inner_content_wrapper">
-          <input type="email" placeholder="Email" />
+          <div>
+            <input
+              type="email"
+              placeholder="Email"
+              {...register('email', {
+                required: 'Email is required',
+                pattern: {
+                  value: /^.+[@]{1}[aA-zZ]+[.]{1}.+$/,
+                  message: 'Invalid email format. example@mail.com'
+                }
+              })}
+            />
+            <p>{errors.email?.message}</p>
+          </div>
         </section>
         <section className="form_inner_content_wrapper">
           <button>Log in</button>
