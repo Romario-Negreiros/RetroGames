d18d28da0577a8d06299af978e6f1bd00b30ca82
Romario Negreiros <nromario482@gmail.com>
2022-06-20 00:29:29 -0300

    feat: add props + dismiss button + styles

---
+++ src/components/Error/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import styles from '@styles/components/error.module.css'
+
+interface Props {
+  error: string
+  setError: (err: string) => void
+}
+
+const Error: React.FC<Props> = ({ error, setError }) => {
+  const dismiss = () => setError('')
+
+  return (
+    <section className={styles.container}>
+      <h1>Error</h1>
+      <p>{error}</p>
+      <button className="button" onClick={dismiss}>Dismiss</button>
+    </section>
+  )
+}
+
+export default Error
