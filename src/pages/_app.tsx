6c64ae3851808090bb176ef850195774028973cf
Romario Negreiros <nromario482@gmail.com>
2022-06-21 22:08:37 -0300

    feat: add authContext Provider component + add state for user + add useEffect to subscribe to any changes in auth state

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import firebase from '../libs/firebase'
+import { handleError } from '@utils/handlers'
 
 import { Layout, Toast } from '../components'
 
 import toastContext, { IToast } from '@contexts/toastContext'
+import authContext, { User } from '@contexts/authContext'
 
 import '@styles/common/reset.css'
 import '@styles/common/variables.css'
@@ -11,19 +14,36 @@ import '@styles/common/global.css'
 import type { AppProps } from 'next/app'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [user, setUser] = React.useState<User | null>(null)
   const [toast, setToast] = React.useState<IToast>({
     isOpened: false,
     message: '',
     timeToCloseInMs: 0
   })
 
+  React.useEffect(() => {
+    const unsubscribe = firebase.auth.instance.onAuthStateChanged(
+      user => {
+        console.log(user)
+        // setUser(user as User)
+      },
+      error => {
+        handleError(error, 'Auth state listener', undefined, setToast)
+      }
+    )
+
+    return () => unsubscribe()
+  }, [])
+
   return (
-    <toastContext.Provider value={{ toast, setToast }}>
-      <Layout>
-        <Toast />
-        <Component {...pageProps} />
-      </Layout>
-    </toastContext.Provider>
+    <authContext.Provider value={{ user, setUser }}>
+      <toastContext.Provider value={{ toast, setToast }}>
+        <Layout>
+          <Toast />
+          <Component {...pageProps} />
+        </Layout>
+      </toastContext.Provider>
+    </authContext.Provider>
   )
 }
 
