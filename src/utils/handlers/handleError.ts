f335bd44aeded417d70ce6993173178d1edd0462
Romario Negreiros <nromario482@gmail.com>
2022-06-21 22:06:26 -0300

    refactor: add setToast argument + create setErrorOrToast function to decide whether setError or setToast will be used to alert about the error

--- src/utils/handlers/handleError.ts
+++ src/utils/handlers/handleError.ts
@@ -1,9 +1,20 @@
 import { joinErrMsgAndAction } from '@utils/helpers'
+import handleToast from './handleToast'
+
+import { IToast } from '@contexts/toastContext'
 
 import { AuthErrorCodes } from 'firebase/auth'
 
-const handleError = (error: unknown, action: string, setError: (error: string) => void) => {
+const handleError = (error: unknown, action: string, setError?: (error: string) => void, setToast?: (toast: IToast) => void) => {
   const defaultUserErrorMessage = 'Looks like something went wrong, if the error persists, try again after a while!'
+
+  const setErrorOrToast = (message: string) => {
+    if (setError) {
+      setError(message)
+    } else if (setToast) {
+      handleToast(message, setToast)
+    }
+  }
 
   if (error instanceof Error) {
     const firebaseError = error.message.match(/[(]{1}[a-z]+[/]{1}[a-z-]+[)]{1}/)
@@ -13,34 +24,37 @@ const handleError = (error: unknown, action: string, setError: (error: string) =
 
       switch (message) {
         case AuthErrorCodes.NETWORK_REQUEST_FAILED:
-          setError('Please, check your internet connection and try again!')
+          setErrorOrToast('Please, check your internet connection and try again!')
           break
         case AuthErrorCodes.INVALID_PASSWORD:
-          setError('Wrong password!')
+          setErrorOrToast('Wrong password!')
           break
         case AuthErrorCodes.EMAIL_EXISTS:
-          setError('This email already exists, try another one!')
+          setErrorOrToast('This email already exists, try another one!')
           break
         case AuthErrorCodes.TOO_MANY_ATTEMPTS_TRY_LATER:
-          setError('Too many attempts, try again later!')
+          setErrorOrToast('Too many attempts, try again later!')
           break
         case AuthErrorCodes.USER_DELETED:
-          setError('This user has been deleted!')
+          setErrorOrToast('This user has been deleted!')
           break
         case AuthErrorCodes.CREDENTIAL_TOO_OLD_LOGIN_AGAIN:
-          setError('Your need to sign in again to complete this operation!')
+          setErrorOrToast('Your need to sign in again to complete this operation!')
+          break
+        case 'auth/invalid-action-code':
+          setErrorOrToast('The link or code has already been used, or has expired!')
           break
         default:
-          setError(defaultUserErrorMessage)
+          setErrorOrToast(defaultUserErrorMessage)
       }
       console.error(joinErrMsgAndAction(message, action))
     } else {
       console.error(joinErrMsgAndAction(error.message, action))
-      setError(error.message)
+      setErrorOrToast(error.message)
     }
   } else {
     console.error(joinErrMsgAndAction('Unknown error', action))
-    setError(defaultUserErrorMessage)
+    setErrorOrToast(defaultUserErrorMessage)
   }
 }
 
