84c15a7c072fe0470d92528bc930ad6958522ebd
Romario Negreiros <nromario482@gmail.com>
2022-06-22 17:24:55 -0300

    feat: export new hook

--- src/utils/hooks/index.ts
+++ src/utils/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as useToast } from './useToast'
 export { default as useAuth } from './useAuth'
 export { default as useAuthMethods } from './useAuthMethods'
+export { default as useFirestore } from './useFirestore'
