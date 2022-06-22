24afee7f0c5bbfbe3f9bc8cde0e64cb269aba4f0
Romario Negreiros <nromario482@gmail.com>
2022-06-21 22:18:07 -0300

    feat: add useEffect to run only once if the url is a sign in with email link + handle errors in useEffect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,41 @@
+import React from 'react'
+import firebase from '../libs/firebase'
+import { useToast, useAuthMethods } from '@utils/hooks'
+import { handleError, handleToast } from '@utils/handlers'
+
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return <div>oi</div>
+  const { setToast } = useToast()
+  const { finishSignInWithEmailLink } = useAuthMethods()
+
+  React.useEffect(() => {
+    const { auth } = firebase
+    if (auth.isSignInWithEmailLink(auth.instance, window.location.href)) {
+      (async () => {
+        try {
+          await finishSignInWithEmailLink()
+        } catch (err) {
+          handleError(
+            err,
+            'Complete log in or account creation with email link',
+            undefined,
+            setToast
+          )
+        }
+      })()
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return (
+    <div>
+      hello world!
+      <button onClick={() => handleToast('toast mano', setToast)}>
+        toast zao
+      </button>
+    </div>
+  )
 }
 
 export default Home
