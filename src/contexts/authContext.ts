f59f6fdd0b0a583c3510709b5501b5be3f2c914f
Romario Negreiros <nromario482@gmail.com>
2022-06-21 22:14:13 -0300

    feat: create auth context + pass initial value object to avoid the need of using optional chaining or not null assertions when accessing auth context value

---
+++ src/contexts/authContext.ts
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import { User as FirebaseUser } from 'firebase/auth'
+
+export interface User extends FirebaseUser {
+  name: string
+}
+
+export interface IAuthContext {
+  user: User | null
+  setUser: (user: User | null) => void
+}
+
+const initialValues = {
+  user: null,
+  setUser: () => {}
+}
+
+const authContext = React.createContext<IAuthContext>(initialValues)
+
+export default authContext
