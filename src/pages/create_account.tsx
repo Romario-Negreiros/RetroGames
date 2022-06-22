75d949361844fa3c0e544dc9b259c2b64a096288
Romario Negreiros <nromario482@gmail.com>
2022-06-21 22:15:59 -0300

    feat: add error and waiting components + add react hook form + verify if email already exists before sending sign in link to email in onSubmit function

--- src/pages/create_account.tsx
+++ src/pages/create_account.tsx
@@ -1,8 +1,13 @@
+import React from 'react'
 import { useForm, SubmitHandler } from 'react-hook-form'
+import { useAuthMethods, useToast } from '@utils/hooks'
+import { handleError, handleToast } from '@utils/handlers'
 
 import Link from 'next/link'
+import { Error as ErrorComponent, Waiting } from '../components'
 
 import type { NextPage } from 'next'
+import { AuthErrorCodes } from 'firebase/auth'
 
 interface Inputs {
   email: string
@@ -10,24 +15,63 @@ interface Inputs {
 }
 
 const CreateAccount: NextPage = () => {
+  const [error, setError] = React.useState('')
+  const [isLoaded, setIsLoaded] = React.useState(true)
+  const { setToast } = useToast()
+  const { sendSignInLinkToEmail, verifyIfEmailAlreadyExists } = useAuthMethods()
+
   const {
     register,
     handleSubmit,
     formState: { errors }
   } = useForm<Inputs>()
 
-  const onSubmit: SubmitHandler<Inputs> = data => {
-    console.log(data)
+  const onSubmit: SubmitHandler<Inputs> = async ({ email, name }) => {
+    try {
+      setIsLoaded(false)
+      if (!(await verifyIfEmailAlreadyExists(email))) {
+        await sendSignInLinkToEmail(email)
+        handleToast(
+          ` Please, click on the link sent to your email to complete the 
+          account creation, make sure to check your spam box too!
+        `,
+          setToast,
+          10000
+        )
+        setIsLoaded(true)
+        const submitBtn = document.querySelector('.button') as HTMLButtonElement
+        submitBtn.disabled = true
+        return
+      }
+      throw new Error(`(${AuthErrorCodes.EMAIL_EXISTS})`)
+    } catch (err) {
+      handleError(err, 'Creating new account', setError)
+      setIsLoaded(true)
+    }
   }
 
+  if (!isLoaded) {
+    return (
+      <section className="full_screen_height_wrapper">
+        <Waiting waitingFor="Creating account..." />
+      </section>
+    )
+  } else if (error) {
+    return (
+      <section className="full_screen_height_wrapper">
+        <ErrorComponent error={error} setError={setError} />
+      </section>
+    )
+  }
   return (
-    <section className="form_wrapper">
+    <section className="full_screen_height_wrapper">
       <form onSubmit={handleSubmit(onSubmit)}>
         <section className="form_inner_content_wrapper">
           <div>
             <input
               type="email"
               placeholder="Email"
+              className="input"
               {...register('email', {
                 required: 'Email is required',
                 pattern: {
@@ -41,6 +85,7 @@ const CreateAccount: NextPage = () => {
           <div>
             <input
               placeholder="Name"
+              className="input"
               {...register('name', {
                 required: 'Name is required',
                 minLength: {
@@ -57,7 +102,7 @@ const CreateAccount: NextPage = () => {
           </div>
         </section>
         <section className="form_inner_content_wrapper">
-          <button>Submit</button>
+          <button className="button">Submit</button>
           <Link href="/login">
             <a>Log in instead</a>
           </Link>
