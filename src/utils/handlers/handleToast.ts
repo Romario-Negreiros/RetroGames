387eab2f04358733296c462eb09e88309461edce
Romario Negreiros <nromario482@gmail.com>
2022-06-21 22:07:26 -0300

    refactor: add 600 milliseconds to setTimeout delay due to toast's transition

--- src/utils/handlers/handleToast.ts
+++ src/utils/handlers/handleToast.ts
@@ -3,7 +3,7 @@ import { IToast } from '@contexts/toastContext'
 const handleToast = (
   message: string,
   setToast: (toast: IToast) => void,
-  timeToCloseInMs: number = 3600 // 3 SECONDS + 600 MILLISECONDS DUE TO TOAST'S TRANSITION
+  timeToCloseInMs: number = 3000 // 3 SECONDS
 ) => {
   setToast({
     isOpened: true,
@@ -12,7 +12,8 @@ const handleToast = (
   })
 
   // + 100 MILLISECONDS TO BE ABLE TO SEE PROGRESS BAR FULLFILLED
-  setTimeout(() => setToast({ isOpened: false, message: '', timeToCloseInMs: 0 }), timeToCloseInMs + 100)
+  // + 600 MILLISECONDS DUE TO TOAST'S TRANSITION
+  setTimeout(() => setToast({ isOpened: false, message: '', timeToCloseInMs: 0 }), timeToCloseInMs + 100 + 600)
 }
 
 export default handleToast
