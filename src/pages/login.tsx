cbdc754672c2e5132da6be9afd224ce454afe831
Romario Negreiros <nromario482@gmail.com>
2022-06-21 22:16:49 -0300

    feat: handle error and isLoaded states + add react hook form + create onSubmit function

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,10 @@
+import React from 'react'
 import { useForm, SubmitHandler } from 'react-hook-form'
+import { useAuthMethods, useToast } from '@utils/hooks'
+import { handleError, handleToast } from '@utils/handlers'
 
 import Link from 'next/link'
+import { Error, Waiting } from '../components'
 
 import type { NextPage } from 'next'
 
@@ -9,24 +13,58 @@ interface Inputs {
 }
 
 const Login: NextPage = () => {
+  const [error, setError] = React.useState('')
+  const [isLoaded, setIsLoaded] = React.useState(true)
+  const { setToast } = useToast()
+  const { sendSignInLinkToEmail } = useAuthMethods()
   const {
     register,
     handleSubmit,
     formState: { errors }
   } = useForm<Inputs>()
 
-  const onSubmit: SubmitHandler<Inputs> = data => {
-    console.log(data)
+  const onSubmit: SubmitHandler<Inputs> = async ({ email }) => {
+    try {
+      setIsLoaded(false)
+      await sendSignInLinkToEmail(email)
+      handleToast(
+        ` Please, click on the link sent to your email to complete the 
+        log in, make sure to check your spam box too!
+        `,
+        setToast,
+        10000
+      )
+      setIsLoaded(true)
+      const submitBtn = document.querySelector('.button') as HTMLButtonElement
+      submitBtn.disabled = true
+    } catch (err) {
+      handleError(err, 'Logging in', setError)
+      setIsLoaded(true)
+    }
   }
 
+  if (!isLoaded) {
+    return (
+      <section className="full_screen_height_wrapper">
+        <Waiting waitingFor="Creating account..." />
+      </section>
+    )
+  } else if (error) {
+    return (
+      <section className="full_screen_height_wrapper">
+        <Error error={error} setError={setError} />
+      </section>
+    )
+  }
   return (
-    <section className="form_wrapper">
+    <section className="full_screen_height_wrapper">
       <form onSubmit={handleSubmit(onSubmit)}>
         <section className="form_inner_content_wrapper">
           <div>
             <input
               type="email"
               placeholder="Email"
+              className="input"
               {...register('email', {
                 required: 'Email is required',
                 pattern: {
@@ -39,7 +77,7 @@ const Login: NextPage = () => {
           </div>
         </section>
         <section className="form_inner_content_wrapper">
-          <button>Log in</button>
+          <button className="button">Log in</button>
           <Link href="create_account">
             <a>Create account</a>
           </Link>
