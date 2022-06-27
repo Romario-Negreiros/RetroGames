bae37fb698174c2101b7c587f2c95d504eb8db9c
Romario Negreiros <nromario482@gmail.com>
2022-06-27 12:46:29 -0300

    refactor: remove handleAuthStateChange and handleClickOnNavItem + remove navItems state and rendering nav items directly to reduce complexity

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,43 +6,13 @@ import MenuIcon from '../MenuIcon'
 import Image from 'next/image'
 import Link from 'next/link'
 
-import { User } from '@contexts/authContext'
-
 import styles from '@styles/components/header.module.css'
-
-interface Item {
-  text: string
-  url?: string
-  disabled?: boolean
-}
-
-const initialNavItems: Item[] = [
-  {
-    text: 'Home',
-    url: '/'
-  },
-  {
-    text: 'user.name',
-    url: '/:name'
-  },
-  {
-    text: 'Sign in',
-    url: '/login',
-    disabled: false
-  },
-  {
-    text: 'Sign out',
-    disabled: true
-  }
-]
 
 const Header: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false)
-  const [navItems, setNavItems] = React.useState(initialNavItems)
   const { user } = useAuth()
   const { setToast } = useToast()
   const { signOut } = useAuthMethods()
-  const firstUpdate = React.useRef(true)
 
   const handleMobileMenu = () => {
     if (typeof window !== 'undefined' && window.innerWidth <= 600) {
@@ -58,29 +28,6 @@ const Header: React.FC = () => {
     }
   }
 
-  const handleClickOnNavItem = (item: Item) => {
-    handleMobileMenu()
-    if (item.text === 'Log off') {
-      handleSignOut()
-    }
-  }
-
-  const handleUserAuthStateChange = React.useCallback((user: User | null) => {
-    const navItemsCopy: Item[] = JSON.parse(JSON.stringify(navItems))
-    navItemsCopy[2].disabled = Boolean(user)
-    navItemsCopy[3].disabled = !user
-    setNavItems(navItemsCopy)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-
-  React.useEffect(() => {
-    if (firstUpdate.current) {
-      firstUpdate.current = false
-    } else {
-      handleUserAuthStateChange(user)
-    }
-  }, [user, handleUserAuthStateChange])
-
   return (
     <header className={styles.container}>
       <section>
@@ -89,23 +36,35 @@ const Header: React.FC = () => {
       <nav>
         <MenuIcon handleMobileMenu={handleMobileMenu} isMenuOpen={isMenuOpen} />
         <ul className={`${styles.nav_list_container} ${isMenuOpen && styles.nav_list_container_active}`}>
-          {navItems.map(item => {
-            if (!item.disabled) {
-              return (
-                <li key={item.text} onClick={() => handleClickOnNavItem(item)}>
-                  {item.url ? (
-                    <Link href={item.url}>
-                      <a>{item.text}</a>
-                    </Link>
-                  ) : (
-                    <a>
-                      {item.text}
-                    </a>
-                  )}
-                </li>
-              )
-            } else return null
-          })}
+          <li onClick={handleMobileMenu}>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          {user && (
+            <li onClick={handleMobileMenu}>
+              <Link href={`/${user.displayName}`}>
+                <a>{user.displayName}</a>
+              </Link>
+            </li>
+          )}
+          {!user && (
+            <li onClick={handleMobileMenu}>
+              <Link href="/sign_in">
+                <a>Sign In</a>
+              </Link>
+            </li>
+          )}
+          {user && (
+            <li
+              onClick={() => {
+                handleMobileMenu()
+                handleSignOut()
+              }}
+            >
+              <a>Sign Out</a>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
