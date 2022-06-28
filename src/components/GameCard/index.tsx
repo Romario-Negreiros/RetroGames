3c39813a738230016814cdad31bf12e7e8a68bdb
Romario Negreiros <nromario482@gmail.com>
2022-06-28 00:02:19 -0300

    feat: start adding article for game's description and image preview

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
 import Link from 'next/link'
+import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import { faInfoCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons'
 
 import styles from '@styles/components/gameCard.module.css'
 
@@ -18,7 +22,10 @@ const GameCard: React.FC<Props> = ({ game: { name, urlName, image, description }
 
   return (
     <li>
-      <h2 className={styles.game}>{name}</h2>
+      <h2>
+        {name} <br />
+        <FontAwesomeIcon icon={faInfoCircle} color="#4ea9ff" size="1x" /> <br />
+      </h2>
       <div className={styles.game_options}>
         <Link href={`/${urlName}/leaderboards`}>
           <a>Leaderboards</a>
@@ -27,6 +34,10 @@ const GameCard: React.FC<Props> = ({ game: { name, urlName, image, description }
           <a>Play</a>
         </Link>
       </div>
+      <article className={styles.game_description}>
+        <Image src={image} alt={name} width="100%" height="40px" layout="responsive" objectFit="contain" />
+        <p>{description}</p>
+      </article>
     </li>
   )
 }
