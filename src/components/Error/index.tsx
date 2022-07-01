c275f5d49535c1013392921922c0f006ae12201e
Romario Negreiros <nromario482@gmail.com>
2022-07-01 15:59:39 -0300

    refactor: make setError prop optional and render button with different onClick handlers conditionally

--- src/components/Error/index.tsx
+++ src/components/Error/index.tsx
@@ -1,22 +1,29 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 
 import styles from '@styles/components/error.module.css'
 
 interface Props {
   error: string
-  setError: (err: string) => void
+  setError?: (err: string) => void
 }
 
 const Error: React.FC<Props> = ({ error, setError }) => {
-  const dismiss = () => setError('')
+  const { back } = useRouter()
 
   return (
     <section className={styles.container}>
       <h1>Error</h1>
       <p>{error}</p>
-      <button className="button" onClick={dismiss}>
-        Dismiss
-      </button>
+      {setError ? (
+        <button className="button" onClick={() => setError('')}>
+          Dismiss
+        </button>
+      ) : (
+        <button className="button" onClick={() => back()}>
+          Go back
+        </button>
+      )}
     </section>
   )
 }
