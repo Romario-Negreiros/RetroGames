e818474ae2e4a488082cafdfa23dadd04f89deb3
Romario Negreiros <nromario482@gmail.com>
2022-07-08 20:37:18 -0300

    feat: add Player interface

--- src/components/TicTacToe/types.d.ts
+++ src/components/TicTacToe/types.d.ts
@@ -9,3 +9,9 @@ export interface CanvasDimensions {
 }
 
 export type GameStates = 'pre game' | 'finding a match' | 'in progress' | 'game ended'
+
+export interface Player {
+  id: 1 | 2
+  name: string
+  shape: 'x' | 'o'
+}
