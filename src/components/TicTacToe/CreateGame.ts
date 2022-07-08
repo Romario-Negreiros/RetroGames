46cb6b4a9bccf1e68936fe279abbc1d57f856247
Romario Negreiros <nromario482@gmail.com>
2022-07-08 20:40:05 -0300

    feat: add setTurn and reset props to CreateGame + add board, players and results variables + add mock data in variables to test functionalities + finish end, start and findAMatch functions + create setMovement and checkBoard functions + refactor getP1 and getP2 functions

--- src/components/TicTacToe/CreateGame.ts
+++ src/components/TicTacToe/CreateGame.ts
@@ -1,44 +1,82 @@
-import type { GameStates } from './types'
+import type { GameStates, Player } from './types'
 
-const CreateGame = (setGameState: (gameState: GameStates) => void) => {
-  let p1: any
-  let p2: any
-  let results: any
+const generateRandomNumberInRange1to2 = () => {
+  return Math.ceil(Math.random() * 2)
+}
+
+const CreateGame = (
+  setGameState: (gameState: GameStates) => void,
+  setTurn: (turn: Player | null) => void,
+  reset: (ctx: HTMLCanvasElement) => void
+) => {
+  const players: { p1: Player | null; p2: Player | null } = {
+    p1: null,
+    p2: null
+  }
+  const results: any = {}
+  let board = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ]
 
   const findAMatch = () => {
     setGameState('finding a match')
 
-    p1 = {
-      name: 'roamrio'
+    players.p1 = {
+      id: 1,
+      name: 'roamrio',
+      shape: generateRandomNumberInRange1to2() === 1 ? 'x' : 'o'
     }
-    p2 = {
-      name: 'oamsoasm'
+    players.p2 = {
+      id: 2,
+      name: 'oamsoasm',
+      shape: players.p1.shape === 'x' ? 'o' : 'x'
     }
   }
 
   const start = () => {
     setGameState('in progress')
+
+    const turn = generateRandomNumberInRange1to2() === 1 ? players.p1 : players.p2
+    return turn
   }
 
   const end = () => {
     setGameState('game ended')
-    results = {
-      winner: p1,
-      loser: p2
+    results.winner = players.p1
+    results.loser = players.p2
+    players.p1 = null
+    players.p2 = null
+    board = [['', '', ''], ['', '', ''], ['', '', '']]
+    setTurn(null)
+    // reset canvas to its initial state
+    const canvas = document.querySelector('canvas') as HTMLCanvasElement
+    reset(canvas)
+  }
+
+  const checkBoard = () => {
+    if (board.every(row => row.every(col => col === 'x' || col === 'o'))) {
+      end()
     }
   }
 
-  const getP1 = () => p1
-  const getP2 = () => p2
+  const setMovement = (row: number, col: number, player: 'p1' | 'p2') => {
+    board[row][col] = players[player]?.shape as string
+    checkBoard()
+  }
+
+  const getP1 = () => players.p1
+  const getP2 = () => players.p2
   const getResults = () => results
 
   return {
     findAMatch,
     start,
-    end,
     getP1,
     getP2,
-    getResults
+    getResults,
+    setMovement
   }
 }
 
