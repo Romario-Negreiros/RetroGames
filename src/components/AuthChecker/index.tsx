44157798d4e93bcb61d8fa786397100671d7cd19
Romario Negreiros <nromario482@gmail.com>
2022-07-24 13:24:42 -0300

    feat: create component to check if user is signed in when accessing one of the pages included in forbiddenPaths array

---
+++ src/components/AuthChecker/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+
+import { Error } from '../'
+
+import type { User } from '@contexts/authContext'
+
+interface Props {
+  children: React.ReactNode
+  user: User | null
+}
+
+const forbiddenPaths = ['/games', '/users']
+
+const AuthChecker: React.FC<Props> = ({ children, user }) => {
+  const { pathname } = useRouter()
+
+  if (!forbiddenPaths.some(path => pathname.includes(path))) {
+    return <>{children}</>
+  } else if (!user) {
+    return (
+      <main className="main_container full_screen_height_wrapper">
+        <Error error={'You need to be signed in to access this page!'} />
+      </main>
+    )
+  }
+  return <>{children}</>
+}
+
+export default AuthChecker
